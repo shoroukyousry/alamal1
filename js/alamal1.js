b44b5c4d01f8483a2bc81f9cbe332223a20c5e93
unknown <Shorouk Yossry>
2023-09-20 17:25:03 +0200

    first commit

---
+++ js/alamal1.js
@@ -0,0 +1,92 @@
+document.getElementById("shoppingcar1");
+let shoppingcar1="shoppingcar1";
+
+
+
+
+
+     shoppingcar1.addEventLisener("click",()=>{
+
+   let pay=document.getElementById("pay");
+   let img1=document.getElementById("img1");
+    pay.appendChild(img1);
+    img1.style.width = "200px";
+    img1.style.height = "150px";
+    shoppingcar1.style.colore = "blue";
+     });
+
+
+
+document.getElementById("img1");sss
+console.log("img1");
+document.getElementById("img2");
+console.log("img2");
+document.getElementById("img3");
+console.log("img3");
+
+document.getElementById("img4");
+console.log("1mg4");
+document.getElementById("img5");
+console.log("img5");
+document.getElementById("img6");
+console.log("img6");
+
+
+document.getElementById("img7");
+console.log("img7");
+document.getElementById("img8");
+console.log("img8");
+document.getElementById("img9");
+console.log("img9");
+
+
+
+
+
+
+
+
+
+
+
+
+/*call back function
+
+function getName(name){
+    return "hellow"+" "+name;
+};
+
+function hellow( name, callback){
+     console.log (callback(name));
+};
+
+
+hellow("shorouk",getName);
+
+
+
+
+
+/*function coull(num1,num2){
+     return  num1 + num2;
+
+};
+
+function sum(num1,num2,callback){
+   
+  console.log(callback(num1,num2));
+};
+
+sum(2,3,coull);
+
+
+
+
+/*const shorouk=["dd","ee","cc"]
+const fruits=["aa","bb","cc"];
+ fruits.join(shorouk);
+ console.log(fruits);*/
+
+
+let phones=["samsong","iphon","nokia"];
+console.log(phones.length);
